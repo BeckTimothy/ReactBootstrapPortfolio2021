b279546c522312966be9f3d84e772eb52915af97
Timothy Beck <johndaveleet@gmail.com>
2020-11-04 15:39:18 -0700

    project is ready for front end development

--- app/src/index.js
+++ app/src/index.js
@@ -5,20 +5,22 @@ import {Route, Switch} from "react-router";
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import {Provider} from 'react-redux';
-import {combinedReducers} from "./shared/reducers/index";
+//import {combinedReducers} from "./shared/reducers/index";
 import {FourOhFour} from "./pages/fourOhFour/FourOhFour";
+import {Home} from "./pages/home/Home.js";
 import {Container, Row, Col} from "react-bootstrap";
 import './styles.css';
 
 
-const store = createStore(combinedReducers, applyMiddleware(thunk));
+const store = createStore(applyMiddleware(thunk));
 const Routing = (store) => (
 	<>
 		<Provider store={store}>
 			<BrowserRouter>
 				<Container fluid>
 					<Row className={'sticky-top'}>
-						<Header/>
+						{/*<Header/>*/}
+						<p>Test</p>
 					</Row>
 					<Row>
 						<Switch>
