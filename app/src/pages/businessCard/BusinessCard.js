dc49c58cb9264b3a596092708506984adde09ae1
Timothy Beck <johndaveleet@gmail.com>
2020-11-07 18:30:48 -0700

    boilerplated businesscard page

---
+++ app/src/pages/businessCard/BusinessCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Col, Row} from "react-bootstrap";
+
+export const BusinessCard = () => {
+
+	/*
+		Notes/planning:
+			Render different page components to the page based off a button choice.
+			Project story; How to Assemble; How to Flash Firmware;
+	*/
+
+
+	return (
+		<>
+			<Row className={""}>
+			</Row>
+		</>
+	)
+};
