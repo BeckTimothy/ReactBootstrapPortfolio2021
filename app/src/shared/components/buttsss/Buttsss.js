c4d171e4956635fa0e2692db217d1f6370973f11
Timothy Beck <barricuda1993@yahoo.com>
2020-11-17 13:10:16 -0700

    modified alt text of 404 gifs

--- app/src/shared/components/buttsss/Buttsss.js
+++ app/src/shared/components/buttsss/Buttsss.js
@@ -48,55 +48,53 @@ import image40 from "../buttsss/img/40.gif";
 //creates random Buttsss for 404 page
 export const Buttsss = () => {
 
-    let displayedImg = <img src={image0} alt="loading..."/>;
+    let displayedImg = <img src={image0} alt="animated image of a hand drawn booty"/>;
     let numOfImg = 41;
     let randImg = Math.floor(Math.random() * Math.floor(numOfImg));
     switch(randImg) {
-        case 0: displayedImg = <img className={`w-75`} src={image0} alt="loading..."/>; break;
-        case 1: displayedImg = <img className={`w-75`} src={image1} alt="loading..."/>; break;
-        case 2: displayedImg = <img className={`w-75`} src={image2} alt="loading..."/>; break;
-        case 3: displayedImg = <img className={`w-75`} src={image3} alt="loading..."/>; break;
-        case 4: displayedImg = <img className={`w-75`} src={image4} alt="loading..."/>; break;
-        case 5: displayedImg = <img className={`w-75`} src={image5} alt="loading..."/>; break;
-        case 6: displayedImg = <img className={`w-75`} src={image6} alt="loading..."/>; break;
-        case 7: displayedImg = <img className={`w-75`} src={image7} alt="loading..."/>; break;
-        case 8: displayedImg = <img className={`w-75`} src={image8} alt="loading..."/>; break;
-        case 9: displayedImg = <img className={`w-75`} src={image9} alt="loading..."/>; break;
-        case 10: displayedImg = <img className={`w-75`} src={image10} alt="loading..."/>; break;
-        case 11: displayedImg = <img className={`w-75`} src={image11} alt="loading..."/>; break;
-        case 12: displayedImg = <img className={`w-75`} src={image12} alt="loading..."/>; break;
-        case 13: displayedImg = <img className={`w-75`} src={image13} alt="loading..."/>; break;
-        case 14: displayedImg = <img className={`w-75`} src={image14} alt="loading..."/>; break;
-        case 15: displayedImg = <img className={`w-75`} src={image15} alt="loading..."/>; break;
-        case 16: displayedImg = <img className={`w-75`} src={image16} alt="loading..."/>; break;
-        case 17: displayedImg = <img className={`w-75`} src={image17} alt="loading..."/>; break;
-        case 18: displayedImg = <img className={`w-75`} src={image18} alt="loading..."/>; break;
-        case 19: displayedImg = <img className={`w-75`} src={image19} alt="loading..."/>; break;
-        case 20: displayedImg = <img className={`w-50`} src={image20} alt="loading..."/>; break;
-        case 21: displayedImg = <img className={`w-75`} src={image21} alt="loading..."/>; break;
-        case 22: displayedImg = <img className={`w-75`} src={image22} alt="loading..."/>; break;
-        case 23: displayedImg = <img className={`w-75`} src={image23} alt="loading..."/>; break;
-        case 24: displayedImg = <img className={`w-75`} src={image24} alt="loading..."/>; break;
-        case 25: displayedImg = <img className={`w-75`} src={image25} alt="loading..."/>; break;
-        case 26: displayedImg = <img className={`w-75`} src={image26} alt="loading..."/>; break;
-        case 27: displayedImg = <img className={`w-75`} src={image27} alt="loading..."/>; break;
-        case 28: displayedImg = <img className={`w-75`} src={image28} alt="loading..."/>; break;
-        case 29: displayedImg = <img className={`w-75`} src={image29} alt="loading..."/>; break;
-        case 30: displayedImg = <img className={`w-75`} src={image30} alt="loading..."/>; break;
-        case 31: displayedImg = <img className={`w-75`} src={image31} alt="loading..."/>; break;
-        case 32: displayedImg = <img className={`w-75`} src={image32} alt="loading..."/>; break;
-        case 33: displayedImg = <img className={`w-75`} src={image33} alt="loading..."/>; break;
-        case 34: displayedImg = <img className={`w-75`} src={image34} alt="loading..."/>; break;
-        case 35: displayedImg = <img className={`w-75`} src={image35} alt="loading..."/>; break;
-        case 36: displayedImg = <img className={`w-75`} src={image36} alt="loading..."/>; break;
-        case 37: displayedImg = <img className={`w-75`} src={image37} alt="loading..."/>; break;
-        case 38: displayedImg = <img className={`w-75`} src={image38} alt="loading..."/>; break;
-        case 39: displayedImg = <img className={`w-75`} src={image39} alt="loading..."/>; break;
-        case 40: displayedImg = <img className={`w-75`} src={image40} alt="loading..."/>; break;
-        default: displayedImg = <img className={`w-75`} src={image0} alt="loading..."/>;
+        case 0: displayedImg = <img className={`w-75`} src={image0} alt="animated image of a hand drawn booty"/>; break;
+        case 1: displayedImg = <img className={`w-75`} src={image1} alt="animated image of a hand drawn booty"/>; break;
+        case 2: displayedImg = <img className={`w-75`} src={image2} alt="animated image of a hand drawn booty"/>; break;
+        case 3: displayedImg = <img className={`w-75`} src={image3} alt="animated image of a hand drawn booty"/>; break;
+        case 4: displayedImg = <img className={`w-75`} src={image4} alt="animated image of a hand drawn booty"/>; break;
+        case 5: displayedImg = <img className={`w-75`} src={image5} alt="animated image of a hand drawn booty"/>; break;
+        case 6: displayedImg = <img className={`w-75`} src={image6} alt="animated image of a hand drawn booty"/>; break;
+        case 7: displayedImg = <img className={`w-75`} src={image7} alt="animated image of a hand drawn booty"/>; break;
+        case 8: displayedImg = <img className={`w-75`} src={image8} alt="animated image of a hand drawn booty"/>; break;
+        case 9: displayedImg = <img className={`w-75`} src={image9} alt="animated image of a hand drawn booty"/>; break;
+        case 10: displayedImg = <img className={`w-75`} src={image10} alt="animated image of a hand drawn booty"/>; break;
+        case 11: displayedImg = <img className={`w-75`} src={image11} alt="animated image of a hand drawn booty"/>; break;
+        case 12: displayedImg = <img className={`w-75`} src={image12} alt="animated image of a hand drawn booty"/>; break;
+        case 13: displayedImg = <img className={`w-75`} src={image13} alt="animated image of a hand drawn booty"/>; break;
+        case 14: displayedImg = <img className={`w-75`} src={image14} alt="animated image of a hand drawn booty"/>; break;
+        case 15: displayedImg = <img className={`w-75`} src={image15} alt="animated image of a hand drawn booty"/>; break;
+        case 16: displayedImg = <img className={`w-75`} src={image16} alt="animated image of a hand drawn booty"/>; break;
+        case 17: displayedImg = <img className={`w-75`} src={image17} alt="animated image of a hand drawn booty"/>; break;
+        case 18: displayedImg = <img className={`w-75`} src={image18} alt="animated image of a hand drawn booty"/>; break;
+        case 19: displayedImg = <img className={`w-75`} src={image19} alt="animated image of a hand drawn booty"/>; break;
+        case 20: displayedImg = <img className={`w-50`} src={image20} alt="animated image of a hand drawn booty"/>; break;
+        case 21: displayedImg = <img className={`w-75`} src={image21} alt="animated image of a hand drawn booty"/>; break;
+        case 22: displayedImg = <img className={`w-75`} src={image22} alt="animated image of a hand drawn booty"/>; break;
+        case 23: displayedImg = <img className={`w-75`} src={image23} alt="animated image of a hand drawn booty"/>; break;
+        case 24: displayedImg = <img className={`w-75`} src={image24} alt="animated image of a hand drawn booty"/>; break;
+        case 25: displayedImg = <img className={`w-75`} src={image25} alt="animated image of a hand drawn booty"/>; break;
+        case 26: displayedImg = <img className={`w-75`} src={image26} alt="animated image of a hand drawn booty"/>; break;
+        case 27: displayedImg = <img className={`w-75`} src={image27} alt="animated image of a hand drawn booty"/>; break;
+        case 28: displayedImg = <img className={`w-75`} src={image28} alt="animated image of a hand drawn booty"/>; break;
+        case 29: displayedImg = <img className={`w-75`} src={image29} alt="animated image of a hand drawn booty"/>; break;
+        case 30: displayedImg = <img className={`w-75`} src={image30} alt="animated image of a hand drawn booty"/>; break;
+        case 31: displayedImg = <img className={`w-75`} src={image31} alt="animated image of a hand drawn booty"/>; break;
+        case 32: displayedImg = <img className={`w-75`} src={image32} alt="animated image of a hand drawn booty"/>; break;
+        case 33: displayedImg = <img className={`w-75`} src={image33} alt="animated image of a hand drawn booty"/>; break;
+        case 34: displayedImg = <img className={`w-75`} src={image34} alt="animated image of a hand drawn booty"/>; break;
+        case 35: displayedImg = <img className={`w-75`} src={image35} alt="animated image of a hand drawn booty"/>; break;
+        case 36: displayedImg = <img className={`w-75`} src={image36} alt="animated image of a hand drawn booty"/>; break;
+        case 37: displayedImg = <img className={`w-75`} src={image37} alt="animated image of a hand drawn booty"/>; break;
+        case 38: displayedImg = <img className={`w-75`} src={image38} alt="animated image of a hand drawn booty"/>; break;
+        case 39: displayedImg = <img className={`w-75`} src={image39} alt="animated image of a hand drawn booty"/>; break;
+        case 40: displayedImg = <img className={`w-75`} src={image40} alt="animated image of a hand drawn booty"/>; break;
+        default: displayedImg = <img className={`w-75`} src={image0} alt="animated image of a hand drawn booty"/>;
     }
-
-    //TODO: force a standardized size for images
 
     return (
         <>
